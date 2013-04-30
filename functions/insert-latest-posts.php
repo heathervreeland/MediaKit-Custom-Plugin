1bead621fe55da1f3f3e5b87d5b8f4556443c1d5
heathervreeland <webmaster@occasionsonline.com>
2013-04-30 11:23:52 -0400

    updated post date

--- functions/insert-latest-posts.php
+++ functions/insert-latest-posts.php
@@ -27,27 +27,33 @@ function insert_latest_posts( $num=0 ) {
 
   foreach($myposts as $apost) {
 
+    // grabbing the post ID
     $post_id = $apost->ID;
 
-    //setup_postdata($post);
+    // let's get the date and format it
+    $post_date = date( 'F d, Y', strtotime( $apost->post_date ));
+
+    // start the html wrap
     $output .=	'<div class="recent-post cf">';
     $output .=	'  <div class="recent-post-image">';
 
+    // output a thumbnail 
     if(has_post_thumbnail( $post_id )) {
 
       $output .=	'    <a href="' . get_permalink( $post_id ) . '">';
       $output .=	get_the_post_thumbnail( $post_id, "medium" );
-      $output .=	'</a>';
+      $output .=	'    </a>';
 
     } else { 
 
       $output .=	'    <img src="' . get_bloginfo('stylesheet_directory') . '/images/occasions-magazine-seal.png" alt="Occasions">';
 
     }
+
     $output .=	'  </div>';
     $output .=	'  <div class="recent-post-content">';
     $output .=	'    <h2><a href="' . get_permalink( $post_id ) . '">' .  get_the_title( $post_id ) . '</a></h2>';
-    $output .=	'    <p class="recent-post-date">' . get_the_date() . '</p>';
+    $output .=	'    <p class="recent-post-date">' . $post_date . '</p>';
     $output .=	get_excerpt_by_id( $post_id ); 
     $output .=	'  </div><!-- end recent-post-content -->';
     $output .=	'</div>';
